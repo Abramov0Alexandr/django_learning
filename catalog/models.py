5d7b641491e1b0cf51a7bf3b9c7d8e57ccab24ba
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-05-24 18:11:44 +0300

    Создание моделей и описание их полей

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    """наименование,
+    описание."""
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Превью', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.FloatField(verbose_name='Цена')
+    create_date = models.DateTimeField(verbose_name='Дата создания')
+    last_change_date = models.DateTimeField(verbose_name='Дата последнего изменения')
