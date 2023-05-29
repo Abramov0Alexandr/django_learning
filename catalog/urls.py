26a06f0760d99e717a228ea8d80e493fa80e68d6
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-05-30 00:03:03 +0300

    Установил параметр name для homepage и contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,6 @@ from catalog.views import homepage, contacts
 
 
 urlpatterns = [
-    path('', homepage),
-    path('contacts/', contacts)
+    path('', homepage, name='homepage'),
+    path('contacts/', contacts, name='contacts')
 ]
