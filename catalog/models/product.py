3c06d4d03ef31334ebece54087850ae0e7aa7f98
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-06-05 17:03:56 +0300

    Добавил verbose_name для Product.category

--- catalog/models/product.py
+++ catalog/models/product.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from catalog.models.category import Category
 
@@ -9,7 +10,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='images/', verbose_name='Превью', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.FloatField(verbose_name='Цена')
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     last_change_date = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
