7f502e15d976480bcfbcb5d609930b8a92e265f6
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-06-21 23:27:00 +0300

    Рефакторинг вызова моделей в контроллерах

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.views import generic
 from catalog.forms import CreatePostForm
 from catalog.models import Product, FashionBlog
 
 
 class IndexListView(generic.ListView):
-    model = Product
+    queryset = Product.objects.all()
     template_name = 'catalog/index.html'
     extra_context = {'title': 'SkyStore', }
 
@@ -26,21 +25,15 @@ def contacts(request):
 
 
 class FashionBlogListView(generic.ListView):
-    model = FashionBlog
+    queryset = FashionBlog.objects.filter(is_published=True)
     template_name = 'catalog/blog.html'
     extra_context = {'title': 'SkyStore Blog'}
 
-    def get_queryset(self):
-        return FashionBlog.objects.filter(is_published=True)
-
 
 class DevelopingPostsListView(generic.ListView):
-    model = FashionBlog
+    queryset = FashionBlog.objects.filter(is_published=False)
     template_name = 'catalog/developing_posts.html'
     extra_context = {'title': 'В подготовке'}
-
-    def get_queryset(self):
-        return FashionBlog.objects.filter(is_published=False)
 
 
 class BlogDetailView(generic.DetailView):
