d4d4044a3ebf2c8b581e469f54a6002c01d04368
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-06-26 22:14:21 +0300

    Рефакторинг шаблонов и url ссылок

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,11 @@ app_name = 'blog'
 
 
 urlpatterns = [
-    path('blog/', views.FashionBlogListView.as_view(), name='blog'),
-    path('blog/<slug:blog_slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
-    path('create/', views.AddPostCreateView.as_view(),  name='blog_create'),
-    path('edit/', views.DevelopingPostsListView.as_view(), name='developing_posts'),
-    path('edit/release/<slug:blog_slug>/', views.ReleasePostUpdateView.as_view(), name='release_post'),
-    path('blog/delete/<slug:blog_slug>', views.PostDeleteView.as_view(), name='post_delete'),
+    path('', views.FashionBlogListView.as_view(), name='blog'),
+    path('developing_posts/', views.DevelopingPostsListView.as_view(), name='developing_posts'),
+    path('create/', views.AddPostCreateView.as_view(), name='blog_create'),
+    path('<slug:blog_slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('edite/<slug:blog_slug>/', views.ReleasePostUpdateView.as_view(), name='release_post'),
+    path('developing_posts/delete/<slug:blog_slug>', views.PostDeleteView.as_view(), name='post_delete'),
 
 ]
