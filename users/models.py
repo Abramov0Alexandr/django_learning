3fa63d078a9a93e19379f626081a19b8513dbb4b
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-07-04 13:07:45 +0300

    Рефакторинг модели User

--- users/models.py
+++ users/models.py
@@ -6,6 +6,10 @@ from blog.models.fashion_blog import NULLABLE
 class User(AbstractUser):
     username = None
 
+    first_name = models.CharField(max_length=25, verbose_name='Имя')
+    last_name = models.CharField(max_length=25, verbose_name='Фамилия')
+    patronymic = models.CharField(max_length=25, verbose_name='Отчество', **NULLABLE)
+
     avatar = models.ImageField(upload_to='images/users', verbose_name='Автар', **NULLABLE)
     phone_number = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
     country = models.CharField(max_length=35, verbose_name='Страна', **NULLABLE)
