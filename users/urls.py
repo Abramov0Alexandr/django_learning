09b741a7d589bd3219f848d65d8ab83e4f5f4c98
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-07-09 12:14:06 +0300

    Написал логику для: регистрации пользователя по почте и паролю; восстановление пользователя на автоматически сгенерированный пароль.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from users.views import RegistrationView, ProfileView
+from django.views.generic import TemplateView
+
+from users.views import RegistrationView, ProfileView, ConfirmView, gen_password
 
 
 app_name = 'users'
@@ -8,6 +10,14 @@ app_name = 'users'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('profile/', ProfileView.as_view(), name='profile_edit')
+    path('verify/<key>/', ConfirmView.as_view(), name='confirm'),
+    path('registration/notification/',
+         TemplateView.as_view(template_name='users/reg_notification.html'), name='notification'),
+    path('reg-success/', TemplateView.as_view(template_name='users/reg_success.html'), name='reg_success'),
+
+    path('profile/', ProfileView.as_view(), name='profile_edit'),
+    path('profile/gen-password', gen_password, name='gen_password')
 ]
+
